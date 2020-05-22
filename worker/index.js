2c32a978e521396a97805e14be954e3c20507e55
tzetzo <tzvetanmarinov@yahoo.com>
2020-05-22 07:03:06 +0300

    Multi-Container App - Dockerizing Multiple Services

---
+++ worker/index.js
@@ -0,0 +1,23 @@
+// this code is used for calculation of Fibonacci number & communication with the Redis Server
+
+const redis = require("redis");
+const keys = require("./keys");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message)));
+});
+
+sub.subscribe("insert");
